3e77bdcadf5c345014fc6d45cb696b0c72a906f7
chenxiuheng <chenxiuheng@gmail.com>
2013-11-04 21:20:44 -0800

    demo

---
+++ src/main/src/github/chenxh/demo/LoginAction.java
@@ -0,0 +1,30 @@
+package github.chenxh.demo;
+
+import org.slf4j.LoggerFactory;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+    /**
+     * serialVersionUID
+     */
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    
+    org.slf4j.Logger logger = LoggerFactory.getLogger(LoginAction.class);
+    
+    @Override
+    public String execute() throws Exception {
+        System.out.println(name);
+        return super.execute();
+    }
+
+    /**
+     * 
+     * @see {@link #name name} 
+     **/
+    public final void setName(String name) {
+        this.name = name;
+    }
+}
